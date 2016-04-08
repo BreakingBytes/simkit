fix Wh units
* remove lots of old comments
* complete docstrings
* remove pq and alt rollup funcs and tests
*add some real tests to test_rollup()

examples/PVPower/formulas/pvpower.py
@@ -38,7 +38,7 @@ def f_daterange(freq, *args, **kwargs):
     return list(rrule.rrule(freq, *args, **kwargs))
 
 
-@UREG.wraps(('Wh', None), ('W', None))
+@UREG.wraps(('watt_hour', None), ('W', None))
 def f_energy(ac_power, times):
     """
     Calculate the total energy accumulated from AC power at the end of each
@@ -65,7 +65,7 @@ def groupby_freq(items, times, freq, wkst='SU'):
     MO, TU, WE, TH, FR, SA and SU.
 
     :param items: items in timeseries
-    :param times:
+    :param times: times corresponding to items
     :param freq: One of the ``dateutil.rrule`` frequency constants
     :type freq: str
     :param wkst: One of the ``dateutil.rrule`` weekday constants
@@ -88,59 +88,15 @@ def groupby_freq(items, times, freq, wkst='SU'):
         yield k, ts
 
 
-# =======================  =================
-# Method                   Benchmark
-# =======================  =================
-# Pint wrapper             5.25[ms]
-# Pint no wrapper          227[ms
-# magnitude w/Quantities   55[ms]
-# magnitude w/Pint         24[ms]
-# Quantities only          675[ms]
-# Pint no wrapper w/ unc   2.82[s]
-
-
-# Pint Wrapper Version (FASTEST)
-# pint>=0.7.2
-# benchmark 5.28[ms] using test_rollup
-# NOTE: without wrap, benchcmark is 227[ms]
-# XXX: Pint only!
-# Does not work with Quantities b/c it can't apply np.sum or np.array
-# stacktrace for np.sum is
-# ValueError: Unable to convert between units of "dimensionless" and "h*W"
-# np.array, np.concatenate and np.append all strip dimensions from quantities
 @UREG.wraps('=A', ('=A', None, None))
 def f_rollup(items, times, freq):
     """
+    Use :func:`groupby_freq` to rollup items
 
-    :param items:
-    :param times:
-    :param freq:
+    :param items: items in timeseries
+    :param times: times corresponding to items
+    :param freq: One of the ``dateutil.rrule`` frequency constants
+    :type freq: str
     """
-    # This works fine with Pint but it is slow.
-
     return [np.sum(item for _, item in ts)
             for _, ts in groupby_freq(items, times, freq)]
-
-
-# magnitude version
-# NOTE: since np.append strips dimensions from quantities
-# XXX: will not propagate uncertainty
-def f_rollup_alt(items, times, freq):
-    rollup = [np.sum(item.magnitude for _, item in ts)
-              for _, ts in groupby_freq(items, times, freq)]
-    return rollup * items.units
-
-
-# Quantites Slow Version
-# XXX: Quantities only
-# XXX: will not propagate uncertainty
-def f_rollup_pq(items, times, freq):
-    rollup = []
-    for _, ts in groupby_freq(items, times, freq):
-        subtotal = 0 * items.units
-        for _, item in ts:
-            subtotal += item
-        rollup.append(subtotal)
-    return rollup * items.units
-
-

examples/PVPower/tests/__init__.py
@@ -2,9 +2,8 @@
 tests for pvpower formulas
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
-import quantities as pq
 import pytz
 import imp
 import os
@@ -30,6 +29,8 @@ finally:
     if MODFID:
         MODFID.close()
 
+MONTHLY_ENERGY = [186000.0, 168000.0, 186000.0, 180000.0, 186000.0, 180000.0,
+                  186000.0, 186000.0, 180000.0, 186000.0, 180000.0, 186000.0]
 
 def test_rollup():
     """
@@ -37,34 +38,18 @@ def test_rollup():
     """
     dtstart = PST.localize(datetime(2007, 1, 1, 0, 0, 0))
     dates = MOD.f_daterange('HOURLY', dtstart=dtstart, count=8761)
-    Pac = 1000. * np.sin(np.arange(24) / np.pi)
+    assert dates[0] == dtstart
+    assert dates[12] == dtstart + timedelta(hours=12)
+    Pac = 1000. * np.sin(np.arange(12) * np.pi / 12.0) ** 2
+    Pac = np.pad(Pac, [6, 6], 'constant')
     Pac = np.append(np.tile(Pac, 365), 0) * UREG.watt
     energy, energy_times = MOD.f_energy(Pac, dates)
-
-    # test using Pint wrapper
-    LOGGER.debug('Pint wrapper')
-    c = clock()
-    MOD.f_rollup(energy, energy_times, 'MONTHLY')
-    LOGGER.debug('elapsed time: %g [ms]', (clock() - c) * 1000.)
-
-    # just test using Quantities
-    energy_pq = energy.magnitude * pq.watt * pq.hour
-    LOGGER.debug('Quantities only')
-    c = clock()
-    MOD.f_rollup_pq(energy_pq, energy_times, 'MONTHLY')
-    LOGGER.debug('elapsed time: %g [ms]', (clock() - c) * 1000.)
-
-    # just using magnitude with Pint
-    LOGGER.debug('magnitude with Pint')
-    c = clock()
-    MOD.f_rollup_alt(energy, energy_times, 'MONTHLY')
-    LOGGER.debug('elapsed time: %g [ms]', (clock() - c) * 1000.)
-
-    # just using magnitude with Quantities
-    LOGGER.debug('magnitude with Quantities')
-    c = clock()
-    MOD.f_rollup_alt(energy_pq, energy_times, 'MONTHLY')
-    LOGGER.debug('elapsed time: %g [ms]', (clock() - c) * 1000.)
+    assert energy.units == UREG.Wh
+    start = clock()
+    monthly_energy = MOD.f_rollup(energy, energy_times, 'MONTHLY')
+    stop = clock()
+    LOGGER.debug('elapsed time: %g [ms]', (stop - start) * 1000.)
+    assert np.allclose(monthly_energy[:12], MONTHLY_ENERGY)
 
 
 if __name__ == "__main__":