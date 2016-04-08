56d41c4463409cb5ef079f2305dbe50ebf924d44
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2016-04-08 01:05:21 -0700

    add irradiance formulae
    * dni_extra, airmass, pressure correctd airmass, pressure
    * add data file to data folder

--- examples/PVPower/formulas/irradiance.py
+++ examples/PVPower/formulas/irradiance.py
@@ -8,11 +8,31 @@ import pvlib
 from circus.core import UREG
 
 
+@UREG.wraps(('W/m**2,'), (None, ))
+def f_dni_extra(times):
+    return pvlib.irradiance.extraradiation(times)
+
+
+@UREG.wraps(('dimensionless', ), ('deg', ))
+def f_airmass(solar_zenith):
+    return pvlib.atmosphere.relativeairmass(solar_zenith)
+
+
+@UREG.wraps(('Pa', ), ('m', ))
+def f_pressure(altitude):
+    return pvlib.atmosphere.alt2pres(altitude)
+
+
+@UREG.wraps(('dimensionless', ), ('dimensionless', 'Pa'))
+def f_am_abs(airmass, pressure):
+    return pvlib.atmosphere.absoluteairmass(airmass, pressure)
+
+
 @UREG.wraps(('W/m**2', 'W/m**2', 'W/m**2'),
             ('deg', 'deg', 'deg', 'deg', 'W/m**2', 'W/m**2', 'W/m**2', 'W/m**2',
              'dimensionless', None))
 def f_total_irrad(surface_tilt, surface_azimuth, solar_zenith, solar_azimuth,
-                  dni, ghi, dhi, extraterrestrial, am_abs, model='perez'):
+                  dni, ghi, dhi, dni_extra, am_abs, model='perez'):
     """
     Calculate total irradiance
 
@@ -31,7 +51,7 @@ def f_total_irrad(surface_tilt, surface_azimuth, solar_zenith, solar_azimuth,
     """
     total_irrad = pvlib.irradiance.total_irrad(
         surface_tilt, surface_azimuth, solar_zenith, solar_azimuth, dni, ghi,
-        dhi, dni_extra=extraterrestrial, airmass=am_abs, model=model
+        dhi, dni_extra=dni_extra, airmass=am_abs, model=model
     )
     poa_global = total_irrad['poa_global']
     poa_direct = total_irrad['poa_direct']
