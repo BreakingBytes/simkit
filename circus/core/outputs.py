58234a512dced2858651464a8583270406ebb3bc
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2016-04-21 13:08:04 -0700

    create meta class first
    * add to __metaclass__ attr of OutputSources so new output sources just subclass that instead of metaclass, like Django
    * rename MetaUserOutputSource to MetaOutputSource
    * make sure MetaOutputSource only used with subclasses of OutputSources
    * pop outputs_path and outputs_file from attr and if not None creae param_file
    * if either outputs_path or outputs_file are None, then pop the remaining attr and store them as parameters except for those starting with underscore
    * use Q_(value, units) instead of multiplication since can't use multiplication with degC units
    * add TODO for Fields, OutputFields and grouping all layers in models together ideas!
    * add test_outputs to tests to check that either param_file or parameters is created correctly

--- circus/core/outputs.py
+++ circus/core/outputs.py
@@ -6,7 +6,7 @@ similar to the data layer, except that output sources are always calculation
 modes.
 """
 
-from circus.core import Registry, UREG
+from circus.core import Registry, UREG, Q_
 import json
 import numpy as np
 import os
@@ -38,6 +38,46 @@ class OutputRegistry(Registry):
         super(OutputRegistry, self).register(new_outputs, **kwargs)
 
 
+class MetaOutputSource(type):
+    """
+    Meta class for user specified output source.
+
+    Setting the ``__metaclass__`` attribute to :class:`MetaUserOutputSource`
+    sets :class:`OutputSources` as the base class, forms the full path to the
+    specified output file on the specified output path and passes it to the
+    constructor as the `param_file` argument.
+
+    Classes must set class attributes `output_path` and `output_file` to the
+    specified output path and file.
+
+    Example::
+
+        class OutputSource(object):
+            __metaclass__ = MetaUserOutputSource
+            output_path = 'user_output_folder'
+            output_file = 'new_user_output_source_param_file.json'
+
+    It's not necessary to subclass :class:`object` is because it is already
+    subclassed by :class:`DataSource`. All bases of :class:`DataSource` will be
+    removed from bases before adding DataSource, so that.
+    """
+    def __new__(cls, name, bases, attr):
+        # use only with OutputSources
+        has_parents = [b for b in bases if isinstance(b, MetaOutputSource)]
+        if not has_parents:
+            return super(MetaOutputSource, cls).__new__(cls, name, bases, attr)
+        outputs_path = attr.pop('outputs_path', None)
+        outputs_file = attr.pop('outputs_file', None)
+        if None not in [outputs_path, outputs_file]:
+            attr['param_file'] = os.path.join(outputs_path, outputs_file)
+        else:
+            attr['parameters'] = dict.fromkeys(k for k in attr.iterkeys()
+                                               if not k.startswith('_'))
+            for k in attr['parameters'].iterkeys():
+                attr['parameters'][k] = attr.pop(k)
+        return super(MetaOutputSource, cls).__new__(cls, name, bases, attr)
+
+
 class OutputSources(object):
     """
     A class for formating output sources.
@@ -46,14 +86,14 @@ class OutputSources(object):
         output source.
     :type param_file: str
     """
-    def __init__(self, param_file):
+    __metaclass__ = MetaOutputSource
+
+    def __init__(self):
         #: parameter file
-        self.param_file = param_file
-        with open(param_file, 'r') as fp:
-            #: parameters from file for outputs
-            self.parameters = json.load(fp)
-        # FYI: mutliple assignments share the same memory!
-        # if `a = b = 0`, then `a` and `b` will point to the same memory!
+        if hasattr(self, 'param_file'):
+            with open(self.param_file, 'r') as fp:
+                #: parameters from file for outputs
+                self.parameters = json.load(fp)
         #: outputs initial value
         self.initial_value = {}
         #: size of outputs
@@ -74,50 +114,13 @@ class OutputSources(object):
             self.isproperty[k] = v.get('isproperty', False)  # True or False
             units = str(v.get('units', ''))  # default is non-dimensional
             # NOTE: np.empty is faster than zeros!
-            self.outputs[k] = np.zeros((1, self.size[k])) * UREG[units]
+            self.outputs[k] = Q_(np.zeros((1, self.size[k])), UREG[units])
             # NOTE: Initial values are assigned and outputs resized when
             # simulation "start" method is called from the model.
 
-# TODO: do something to remove redundant class defs here: Subclass all? use
-# metaclasses?
-# TODO: for metaclasses move param_file to class attr
 
-
-class MetaUserOutputSource(type):
-    """
-    Meta class for user specified output source.
-
-    Setting the ``__metaclass__`` attribute to :class:`MetaUserOutputSource`
-    sets :class:`OutputSources` as the base class, forms the full path to the
-    specified output file on the specified output path and passes it to the
-    constructor as the `param_file` argument.
-
-    Classes must set class attributes `output_path` and `output_file` to the
-    specified output path and file.
-
-    Example::
-
-        class NewOutputSource():
-            __metaclass__ = MetaUserOutputSource
-            output_path = 'user_output_folder'
-            output_file = 'new_user_output_source_param_file.json'
-
-    It's not necessary to subclass :class:`object` is because it is already
-    subclassed by :class:`DataSource`. All bases of :class:`DataSource` will be
-    removed from bases before adding DataSource, so that.
-    """
-    def __new__(cls, name, bases, attr):
-        # must use __new__ b/c changing `bases`, can't be done w/ `__init__`
-        # filter out bases from which OutputSources is derived
-        bases = tuple([b for b in bases if not b in OutputSources.__bases__])
-        # must be subclass of OutputSources
-        if OutputSources not in bases:
-            bases += (OutputSources,)  # add OutputSources to bases
-        param_file = os.path.join(attr['outputs_path'], attr['outputs_file'])
-
-        def __init__(self):
-            OutputSources.__init__(self, param_file)
-
-        attr['__init__'] = __init__  # add `__init__` attribute to class
-        return super(MetaUserOutputSource, cls).__new__(cls, name, bases, attr)
-    # omit `__init__` since same signature as `__new__`
+# TODO: create a fields module with a Field base class (and MetaField if necessary)
+# TODO: create an OutputField class for outputs with attributes initial_value,
+# size, uncertainty, isconstant, isproperty and the value.
+# TODO: use OutputField instead of dictionaries
+# TODO: just combine Outputs with other layers in Model class like Django
---
+++ circus/tests/test_outputs.py
@@ -0,0 +1,46 @@
+"""
+test outputs
+"""
+
+from nose.tools import ok_, eq_
+from circus.core.outputs import OutputSources
+import os
+
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+PVPOWER_PATH = os.path.join(DIRNAME, '..', '..', 'examples', 'PVPower')
+
+
+def test_outputs_metaclass():
+
+    class OutputsSourceTest1(OutputSources):
+        outputs_file = 'pvpower.json'
+        outputs_path = os.path.join(PVPOWER_PATH, 'outputs')
+
+    out_src_test1 = OutputsSourceTest1()
+    ok_(isinstance(out_src_test1, OutputSources))
+    eq_(out_src_test1.param_file,
+        os.path.join(PVPOWER_PATH, 'outputs', 'pvpower.json'))
+
+    class OutputsSourceTest2(OutputSources):
+        HourlyEnergy = {"units": "W*h", "init": 0, "size": 8760}
+        MonthlyEnergy = {"units": "W*h", "init": 0, "size": 12}
+        AnnualEnergy = {"units": "W*h", "init": 0}
+        Pac = {"units": "W", "init": 0, "size": 8760}
+        Isc = {"units": "A", "size": 8760}
+        Imp = {"units": "A", "size": 8760}
+        Voc = {"units": "V", "size": 8760}
+        Vmp = {"units": "V", "size": 8760}
+        Pmp = {"units": "W", "size": 8760}
+        Ee = {"units": "dimensionless", "size": 8760}
+        Tcell = {"units": "degC", "size": 8760}
+        poa_global = {"units": "W/m**2", "size": 8760}
+        poa_direct = {"units": "W/m**2", "size": 8760}
+        poa_diffuse = {"units": "W/m**2", "size": 8760}
+        aoi = {"units": "deg", "size": 8760}
+        solar_zenith = {"units": "deg", "size": 8760}
+        solar_azimuth = {"units": "deg", "size": 8760}
+
+    out_src_test2 = OutputsSourceTest2()
+    ok_(isinstance(out_src_test2, OutputSources))
+    for k, v in out_src_test2.parameters.iteritems():
+        eq_(out_src_test1.parameters[k], v)
