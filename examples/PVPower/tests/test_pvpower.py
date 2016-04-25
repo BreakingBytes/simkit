8d9100d3f4622ecd8cefec1ce5c320c2fc4168dc
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2016-04-25 12:14:33 -0700

    update test_pvpower to use Formula classes
    * add note to add circus and/or pvpower to pythonpath before running tests!
    * remove imp module and all MODNAME, MODPATH, MODFID, MODINFO, MODFN and MOD constants
    * remove imp.load_module() and close()
    * instead import PVPowerFormulas from pvpower.formulas and instantiate it as FORMULAS
    * use FORMULAS['<function name>']() everywhere instead of MOD.<function name>()
    * change Pac to ac_power
    * return some stuff from test and main method so we can interact with it
    * tile ac_energy  (365,) and append [0] to conform to np.tile() and np.append() expected args and make IDE happy

--- examples/PVPower/tests/test_pvpower.py
+++ examples/PVPower/tests/test_pvpower.py
@@ -1,56 +1,48 @@
 """
-tests for pvpower formulas
+Tests for pvpower formulas
 """
 
 from datetime import datetime, timedelta
 import numpy as np
 import pytz
-import imp
-import os
 from time import clock
 import logging
+# XXX: add circus to python path for testing!
 from circus.core import UREG
+# XXX: add pvpower to python path for testing!
+from pvpower.formulas import PVPowerFormulas
 
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 PST = pytz.timezone('US/Pacific')
-MODNAME = 'pvpower'
-BASEDIR = os.path.dirname(__file__)
-LOGGER.debug('base dir:\n> %s', BASEDIR)
-MODPATH = os.path.abspath(os.path.join(BASEDIR, '..', 'formulas'))
-LOGGER.debug('path:\n> %s', MODPATH)
-MODFID, MODFN, MODINFO = None, None, None
+FORMULAS = PVPowerFormulas()
 
-try:
-    MODFID, MODFN, MODINFO = imp.find_module(MODNAME,[MODPATH])
-    MOD = imp.load_module(MODNAME, MODFID, MODFN, MODINFO)
-finally:
-    if MODFID:
-        MODFID.close()
 
 MONTHLY_ENERGY = [186000.0, 168000.0, 186000.0, 180000.0, 186000.0, 180000.0,
                   186000.0, 186000.0, 180000.0, 186000.0, 180000.0, 186000.0]
+
 
 def test_rollup():
     """
     test rollup
     """
     dtstart = PST.localize(datetime(2007, 1, 1, 0, 0, 0))
-    dates = MOD.f_daterange('HOURLY', dtstart=dtstart, count=8761)
+    dates = FORMULAS['f_daterange']('HOURLY', dtstart=dtstart, count=8761)
     assert dates[0] == dtstart
     assert dates[12] == dtstart + timedelta(hours=12)
-    Pac = 1000. * np.sin(np.arange(12) * np.pi / 12.0) ** 2
-    Pac = np.pad(Pac, [6, 6], 'constant')
-    Pac = np.append(np.tile(Pac, 365), 0) * UREG.watt
-    energy, energy_times = MOD.f_energy(Pac, dates)
+    ac_power = 1000. * np.sin(np.arange(12) * np.pi / 12.0) ** 2
+    ac_power = np.pad(ac_power, [6, 6], 'constant')
+    ac_power = np.append(np.tile(ac_power, (365,)), [0]) * UREG.watt
+    energy, energy_times = FORMULAS['f_energy'](ac_power, dates)
     assert energy.units == UREG.Wh
     start = clock()
-    monthly_energy = MOD.f_rollup(energy, energy_times, 'MONTHLY')
+    monthly_energy = FORMULAS['f_rollup'](energy, energy_times, 'MONTHLY')
     stop = clock()
     LOGGER.debug('elapsed time: %g [ms]', (stop - start) * 1000.)
     assert np.allclose(monthly_energy[:12], MONTHLY_ENERGY)
+    return dates, ac_power, energy, energy_times, monthly_energy
 
 
 if __name__ == "__main__":
-    test_rollup()
+    results = test_rollup()
