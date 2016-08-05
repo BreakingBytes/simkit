8257c8ad82cbaf1595202a32efad41112e8682a5
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2016-08-05 03:38:01 -0700

    bump to v0.2.3

--- carousel/__init__.py
+++ carousel/__init__.py
@@ -7,5 +7,5 @@ Mark Mikofski (c) 2015
 __author__ = u'Mark Mikofski'
 __email__ = u'mark.mikofski@sunpowercorp.com'
 __url__ = u'https://github.com/SunPower/Carousel'
-__version__ = u'0.2.2'
-__release__ = u'Balancing Act'
+__version__ = u'0.2.3'
+__release__ = u'Balloons'
